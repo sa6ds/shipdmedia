3b20e2e2c79f757b104a21d75b95915a05c0c9c0
saad <96222718+sa6ds@users.noreply.github.com>
2026-05-06 20:16:59 -0400

    Update email link to new address

--- components/contract.tsx
+++ components/contract.tsx
@@ -32,7 +32,7 @@ export function Contract() {
               </Link>
             </Button>
             <Button asChild size="sm">
-              <Link href="mailto:shipdmedia@gmail.com">[Email Us]</Link>
+              <Link href="mailto:saad@teamshipdmedia.com">[Email Us]</Link>
             </Button>
           </div>
         </div>
